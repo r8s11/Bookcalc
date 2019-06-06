4a304734a705a725050697f0444bee08b853a6b3
Roosevelt <roosevelt.bseg@gmail.com>
2019-06-06 17:23:55 -0400

    Printing format is being worked on

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("Hello Wolrd")
+
+row = 9
+column = 3
+bookPages = 21
+
+counter = 1
+for i in range (row):
+
+    for j in range (column):
+        print("Day" , str(counter) + ":", bookPages , "Pages", end = " \t")
+        counter += 1
+    print('\n')
