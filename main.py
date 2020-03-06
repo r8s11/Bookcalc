8c27727ea50dc8a46d9bc6e845e76b354b974a98
Segura <RSEGURA@mgh.harvard.edu>
2020-03-06 16:27:12 -0500

    functional

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 
-bp_input = int(input("How many pages is the book?"))
-days_input = int(input("How many days to read? "))
+bp_input = int(input("How many pages is the book?\n"))
+days_input = int(input("How many days to read?\n"))
+print("")
 
 row = round(days_input / 3) 
 column = 3
 bookPages = (bp_input / days_input)
 
 num_of_day = 1
-for i in range(row):
-    
-    for j in range(column):
-        
+
+while num_of_day <= (days_input):
+    for j in range(3):
         print("Day" , str(num_of_day) + ":", round(bookPages) , "Pages", end = " \t")
         num_of_day += 1
         if num_of_day > days_input:
             break
-
-            
-       
     print('\n')
